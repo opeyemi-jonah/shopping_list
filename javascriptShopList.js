9fb3abd4be48298c18834ebcc2233a6381a79769
Opeyemi Jonah <60982863+opeyemi-jonah@users.noreply.github.com>
2020-02-21 09:51:32 -0700

    shopping list files

---
+++ javascriptShopList.js
@@ -0,0 +1,60 @@
+window.onload = function() {
+		initShoppingList();
+};
+
+function initShoppingList(){
+	let form = document.getElementById("item-form");
+ 	form.addEventListener("submit", (event)=>{
+ 		handleItemForm(event, form);
+
+	});
+}
+
+function handleItemForm(event, formRef) {
+	if(event.preventDefault) {
+		event.preventDefault();
+	}
+	 addItemToShoppingList();
+	formRef.reset();
+
+return false;
+}
+
+function addItemToShoppingList(){
+
+	let itemName = document.getElementById("item-name");
+	let itemAmount = document.getElementById("item-amount");
+	let id = getRandomInt(0,10000000);
+
+	// Creates List item html and appends to page
+	let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
+	let itemListRef = document.getElementById("shopping-list");
+	console.log("Item HTML: ",itemHtml);
+	itemListRef.insertAdjacentHTML("afterend", itemHtml);
+	setDeleteButtonEvent(id);
+
+}
+function setDeleteButtonEvent(id) {
+	let deleteButton = document.getElementById("button"+id);
+	deleteButton.addEventListener("click", () =>{
+		console.log("DeleteButton works"+id);
+		removeListItem(id);
+
+	});
+
+}
+
+function createListItemHtml(itemName,itemAmount, id) {
+	return `<li id="item${id}"> ${itemName} - ${itemAmount} 
+					<button id="button${id}" type="button">Delete Item</button>
+</li>` ;
+}
+function removeListItem(id) {
+	let listItem = document.getElementById("item"+id);
+	listItem.parentNode.removeChild(listItem);
+}
+function getRandomInt(min,max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max-min))+ min;
+}
